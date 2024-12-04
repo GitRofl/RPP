1e49e35910ef104a19a157b000b2048f0ac073b5
Artem Basman <125716203+GitRofl@users.noreply.github.com>
2024-12-04 21:31:15 +0700

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,14 +1,21 @@
-import pytest
+import unittest
 from main import calculate
 
 
-def test_positive_discriminant():
-    assert calculate(1, 5, 6) == 1
+class TestCalculate(unittest.TestCase):
+
+    
+    def test_positive_discriminant(self):
+        self.assertEqual(calculate(1, 5, 6), 1)
+
+    
+    def test_zero_discriminant(self):
+        self.assertEqual(calculate(1, 2, 1), 0)
+
+    
+    def test_negative_discriminant(self):
+        self.assertEqual(calculate(1, 2, 3), -8)
 
 
-def test_zero_discriminant():
-    assert calculate(1, 2, 1) == 0
-
-
-def test_negative_discriminant():
-    assert calculate(1, 2, 3) == -8
+if __name__ == '__main__':
+    unittest.main()
