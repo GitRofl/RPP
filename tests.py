9059a2387b6bde204aac3b7bcd64bbd5684acb55
Artem Basman <125716203+GitRofl@users.noreply.github.com>
2024-12-04 21:11:13 +0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import pytest
 from main import calculate
 
 def test_positive_discriminant():
-    assert calculatet(1, 5, 6) == 1  
+    assert calculate(1, 5, 6) == 1  
 
 def test_zero_discriminant():
     assert calculate(1, 2, 1) == 0  
